c7f5a66db6a35705283851f236189983f81f974a
fahimpjee1988 <adjmal.ra@gmail.com>
2020-07-08 16:30:10 +0200

    counter1.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,40 +1,40 @@
 function reset() {
-    console.log("reset")
+    console.log("test")
     document.getElementById("result").innerHTML = "0"
 }
 let x = 0
 function plusOne() {
-    console.log("+1")
+    console.log("+test")
     x += 1
     document.getElementById("result").innerHTML = x
 }
 function minusone() {
-    console.log("-1")
+    console.log("+test")
     x -= 1
     document.getElementById("result").innerHTML = x
 }
 function plus10() {
-    console.log("-1")
+    console.log("+test")
     x += 10
     document.getElementById("result").innerHTML = x
 }
 function minus10() {
-    console.log("-10")
+    console.log("+test")
     x -= 10
     document.getElementById("result").innerHTML = x
 }
 function plushundert() {
-    console.log("+100")
+    console.log("+test")
     x += 100
     document.getElementById("result").innerHTML = x
 }
 function minushundert() {
-    console.log("-100")
+    console.log("+test")
     x -= 100
     document.getElementById("result").innerHTML = x
 }
 function double() {
-    console.log("*2")
+    console.log("+test")
     x *= 2
     document.getElementById("result").innerHTML = x
 }
